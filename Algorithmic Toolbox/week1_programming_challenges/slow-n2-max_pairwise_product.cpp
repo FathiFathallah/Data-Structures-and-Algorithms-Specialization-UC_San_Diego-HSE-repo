41a88e6e64efbca8aa61832ca37a2f5dfd2df00a
Fathi Fathallah Ali <97547912+FathiFathallah@users.noreply.github.com>
2022-01-21 08:45:43 +0200

    Create slow-n2-max_pairwise_product.cpp

---
+++ Algorithmic Toolbox/week1_programming_challenges/slow-n2-max_pairwise_product.cpp
@@ -0,0 +1,32 @@
+//First Problem : Running Time = N^2 (Very Slow) if array with n = 10^5 then the running time will equal to 10^10 ! => solved by finding two maximum
+//SECOND PROBLEM : when the value of the product is bigger than 410065408 (INTEGER OVER FLOW) => Solved by long long
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int MaxPairwiseProduct(const std::vector<int>& numbers) {
+    int max_product = 0;
+    int n = numbers.size();
+
+    for (int first = 0; first < n; ++first) {
+        for (int second = first + 1; second < n; ++second) {
+            max_product = std::max(max_product,
+                numbers[first] * numbers[second]);
+        }
+    }
+
+    return max_product;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> numbers[i];
+    }
+
+    std::cout << MaxPairwiseProduct(numbers); << "\n";
+    return 0;
+}
+
