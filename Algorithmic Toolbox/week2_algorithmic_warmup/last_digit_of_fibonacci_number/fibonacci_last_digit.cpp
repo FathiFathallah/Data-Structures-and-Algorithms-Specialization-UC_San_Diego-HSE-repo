aa4362e1cefa82fd6a7b00f39e751c4b9a543afb
Fathi Fathallah Ali <97547912+FathiFathallah@users.noreply.github.com>
2022-01-21 10:23:17 +0200

    Update and rename Algorithmic Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp to Algorithmic Toolbox/week2_algorithmic_warmup/last_digit_of_fibonacci_number/fibonacci_last_digit.cpp

---
+++ Algorithmic Toolbox/week2_algorithmic_warmup/last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -0,0 +1,27 @@
+//WORKS WITH BIG NUMBERS LIKE get_fibonacci_last_digit_naive(250)
+//F250 = 7896325826131730509282738943634332893686268675876375
+//WHAT WE ONLY NEED IS THE LAST DIGIT OF THE NUMBER, SO WE DON'T NEED TO SUM THE WHOLE NUMBER ☺
+#include <iostream>
+
+int get_fibonacci_last_digit_naive(int n) {
+	if (n <= 1)
+		return n;
+
+	long long previous = 0;
+	long long current = 1;
+
+	for (int i = 0; i < n - 1; ++i) {
+		long long tmp_previous = previous;
+		previous = current;
+		current = ((tmp_previous % 10) + (current % 10)) % 10;
+	}
+	return current;
+}
+
+int main() {
+	int n;
+	std::cin >> n;
+
+	long long c = get_fibonacci_last_digit_naive(n);
+	std::cout << c << '\n';
+}
